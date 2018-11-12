fa7be8dc56e624845d43a1f266882bd68ef78a2d
Pluies <florent.delannoy@gmail.com>
2018-11-12 08:03:58 +0000

    Updated cluster

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,51 +1,67 @@
+# Configure the Google provider
 provider "google" {
   project = "${var.project}"
   region  = "europe-west1"
 }
 
+# Set up the cluster and its node pool
 resource "google_container_cluster" "cluster" {
-  name                 = "standard-cluster-1"
-  zone                 = "europe-west1"
-  initial_node_count   = 0
-
-  ip_allocation_policy = {
-    cluster_ipv4_cidr_block = "10.32.0.0/14"
-  }
+  name               = "kubernetes-cluster"
+  zone               = "europe-west1"
+  min_master_version = "1.11"
 
   master_authorized_networks_config {}
 
   additional_zones = [
+    "europe-west1-d",
     "europe-west1-b",
     "europe-west1-c",
-    "europe-west1-d",
   ]
+
+  # Disable addons for cost-savings
+  addons_config {
+    http_load_balancing {
+      disabled = true
+    }
+    kubernetes_dashboard {
+      disabled = true
+    }
+  }
+
+  # Disable StackDriver / logging
+  logging_service = "none"
 
   master_auth {
     username = "${var.kube_username}"
     password = "${var.kube_password}"
   }
 
-  node_config {
-    disk_size_gb = 10
-    preemptible  = true
-    machine_type = "f1-micro"
+  remove_default_node_pool = true
 
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-      "https://www.googleapis.com/auth/service.management.readonly",
-      "https://www.googleapis.com/auth/servicecontrol",
-      "https://www.googleapis.com/auth/trace.append",
-    ]
+  node_pool {
+    # Per-zone node count
+    node_count = 1
+
+    management {
+      auto_repair  = true
+      auto_upgrade = true
+    }
+
+    node_config {
+      disk_size_gb = 10
+      preemptible  = true
+      machine_type = "f1-micro"
+
+      oauth_scopes = [
+        "https://www.googleapis.com/auth/devstorage.read_only",
+        "https://www.googleapis.com/auth/logging.write",
+        "https://www.googleapis.com/auth/monitoring",
+        "https://www.googleapis.com/auth/service.management.readonly",
+        "https://www.googleapis.com/auth/servicecontrol",
+        "https://www.googleapis.com/auth/trace.append",
+      ]
+    }
   }
-}
-
-resource "google_container_node_pool" "np" {
-  name       = "default-pool"
-  region     = "europe-west1"
-  cluster    = "${google_container_cluster.cluster.name}"
-  node_count = 1
 }
 
 # The following outputs allow authentication and connectivity to the GKE Cluster.
